e73c77bd74a7ae2d8b1ba458fd59d4588d53662e
Henrique Siqueira <henrique.ribeiro1201@gmail.com>
2026-09-04 21:22:28 -0300

    Add classes for Transporte, Onibus, Metro, and drivers

--- aulasPython/ac2-listaExercicios/exercicio5-ac2.py
+++ aulasPython/ac2-listaExercicios/exercicio5-ac2.py
@@ -1,1 +1,36 @@
+class Transporte:
+    def __init__(self, linha, capacidade):
+        self.linha = linha
+        self.capacidade = capacidade
 
+    def exibir_informacoes(self):
+        print(f"Linha: {self.linha}")
+        print(f"Capacidade: {self.capacidade} passageiros")
+
+class Onibus(Transporte):
+    def __init__(self, linha, capacidade):
+        super().__init__(linha, capacidade)
+
+    def transportar(self):
+        print(f"Ônibus está transportando passageiros")
+
+class Metro(Transporte):
+    def __init__(self, linha, capacidade):
+        super().__init__(linha, capacidade)
+
+    def transportar(self):
+        print(f"Este metrô está transportando passageiros")
+
+class Motorista(Onibus):
+    def __init__(self, linha, capacidade):
+        super().__init__(linha, capacidade)
+
+    def dirigir(self):
+        print(f"O motorista está dirigindo o ônibus da linha {self.linha}")
+
+class Condutor(Metro):
+    def __init__(self, linha, capacidade):
+        super().__init__(linha, capacidade)
+
+    def conduzir(self):
+        print(f"O condutor está conduzindo o metrô da linha {self.linha}")
