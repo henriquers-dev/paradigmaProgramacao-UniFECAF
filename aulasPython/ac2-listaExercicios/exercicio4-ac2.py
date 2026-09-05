58f6eda02d71c0ed05b74d2f703ed0e0bc5bf2dd
Henrique Siqueira <henrique.ribeiro1201@gmail.com>
2026-09-04 21:21:45 -0300

    Update exercicio4-ac2.py

--- aulasPython/ac2-listaExercicios/exercicio4-ac2.py
+++ aulasPython/ac2-listaExercicios/exercicio4-ac2.py
@@ -1,1 +1,26 @@
+class Personagem:
+    def __init__(self, nome, vida):
+        self.nome = nome
+        self.vida = vida
 
+    def exibir_informacoes(self):
+        print(f"Nome: {self.nome}")
+        print(f"Vida: {self.vida}")
+
+class Guerreiro(Personagem):
+    def __init__(self, nome, vida):
+        super().__init__(nome, vida)
+
+    def atacar(self):
+        print(f"{self.nome} realiza um ataque poderoso!")
+
+class Mago(Personagem):
+    def __init__(self, nome, vida):
+        super().__init__(nome, vida)
+
+    def atacar(self):
+        print(f"{self.nome} lança um feitiço mágico!")
+
+
+guerreiro = Guerreiro("Draco", 100)
+mago = Mago("Dumbledore", 80)
