31c3b36c453044ffa2affa182e24e77b5bfb529a
Henrique Siqueira <henrique.ribeiro1201@gmail.com>
2026-09-18 19:33:24 -0300

    Implement payment classes and finalize function
    Added payment classes for Pix, Cartao, and Dinheiro, implementing the pagar method. Included a function to finalize payments.

--- aulasPython/SOLID/LSP/tiposPagamento.py
+++ aulasPython/SOLID/LSP/tiposPagamento.py
@@ -1,1 +1,27 @@
+class Pagamento:
+    def pagar(self, valor):
+        print(f"Pagamento de R$ {valor}")
 
+class Pix(Pagamento):
+    def pagar(self, valor):
+        print(f"Pix de R$ {valor}")
+
+
+class Cartao(Pagamento):
+    def pagar(self, valor):
+        print(f"Cartão de R$ {valor}")
+
+class Dinheiro(Pagamento):
+    def pagar(self, valor):
+        raise Exception("Não suportado.")
+
+def finalizar_pagamento(pagamento, valor):
+    pagamento.pagar(valor)
+
+pix = Pix()
+cartao = Cartao()
+dinheiro = Dinheiro()
+
+finalizar_pagamento(pix, 200)
+finalizar_pagamento(cartao, 200)
+finalizar_pagamento(dinheiro, 200)
